c0e8366e3e36259d08ed38497f6bff176e85429c
Jinx OTP <erler.eliza@gmail.com>
2026-01-28 21:07:58 +0100

    Implemented Scorebord and ball movement.

--- core/src/main/java/com/jinx/otp/GameScreen.java
+++ core/src/main/java/com/jinx/otp/GameScreen.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class GameScreen implements Screen {
@@ -16,13 +17,16 @@ public class GameScreen implements Screen {
     final float BALL_WIDTH = 0.25f;
     final float BALL_HEIGHT = 0.25f;
 
-    final float RACKET_SPEEED = 1f;
+    final float RACKET_SPEEED = 2f;
     final float BALL_SPEED = 0.5f;
 
     final Pong game;
 
     PlayerDirection playerOneDirection;
     PlayerDirection playerTwoDirection;
+
+    float ballVelocityX;
+    float ballVelocityY;
 
     Texture racketTexture;
     Texture ballTexture;
@@ -31,11 +35,17 @@ public class GameScreen implements Screen {
     Sprite racketPlayerTwoSprite;
     Sprite ballSprite;
 
+    int pointsPlayerOne = 0;
+    int pointsPlayerTwo = 0;
+
     public GameScreen(Pong game) {
         this.game = game;
         final String racketTextureFileName = "pong-racket.png";
         racketTexture = new Texture(racketTextureFileName);
 
+        ballVelocityX = 1f;
+        ballVelocityY = 0.5f;
+    
         setupPlayerOneRacket();
         setupPlayerTwoRacket();
         setupBall();
@@ -73,10 +83,7 @@ public class GameScreen implements Screen {
         ballTexture = new Texture(ballTextureFileName);
         ballSprite = new Sprite(ballTexture);
         ballSprite.setSize(BALL_WIDTH, BALL_HEIGHT);
-        final float startX = game.viewport.getWorldWidth() / 2 - 0.5f * BALL_WIDTH;
-        final float startY = game.viewport.getWorldHeight() / 2 - 0.5f * BALL_HEIGHT;
-        ballSprite.setX(startX);
-        ballSprite.setY(startY);
+        resetBallPosition();
     }
 
     @Override
@@ -118,8 +125,12 @@ public class GameScreen implements Screen {
     }
 
     private void logic(float delta) {
+        moveRackets(delta);
+        moveBall(delta);
+    }
+
+    private void moveRackets(float delta) {
         final float racketSpeed = RACKET_SPEEED * delta;
-        final float ballSpeed = BALL_SPEED * delta;
 
         final float maxRacketY = game.WORLD_HEIGHT - RACKET_HEIGHT;
         final float minRacketY = 0f;
@@ -151,6 +162,74 @@ public class GameScreen implements Screen {
                 MathUtils.clamp(racketPlayerTwoSprite.getY(), minRacketY, maxRacketY));
     }
 
+    private void moveBall(float delta) {
+        final float ballSpeedX = ballVelocityX * delta;
+        final float ballSpeedY = ballVelocityY * delta;
+
+        final float ballMaxY = game.WORLD_HEIGHT - BALL_HEIGHT;
+        final float ballMinY = 0f;
+
+        ballSprite.translate(ballSpeedX, ballSpeedY);
+
+        ballSprite.setY(MathUtils.clamp(ballSprite.getY(), ballMinY, ballMaxY));
+
+        if (ballMaxY == ballSprite.getY() || ballMinY == ballSprite.getY()) {
+            ballVelocityY *= -1;            
+        }
+
+        if (isBallOutOfLeftBound()) {
+            ++pointsPlayerTwo;
+            resetBallPosition();
+        } else if (isBallOutOfRightBound()) {
+            ++pointsPlayerOne;
+            resetBallPosition();
+        }
+        
+
+        if (isBallGoingToPlayerOne()) {
+            checkRacketHit(racketPlayerOneSprite);
+        } else {
+            checkRacketHit(racketPlayerTwoSprite);            
+        }
+    }
+
+    private boolean isBallOutOfLeftBound() {
+        return -BALL_WIDTH >= ballSprite.getX();
+    }
+
+    private boolean isBallOutOfRightBound() {
+        return game.WORLD_WIDTH <= ballSprite.getX();
+    }
+
+    private void resetBallPosition() {
+        final float startX = game.viewport.getWorldWidth() / 2 - 0.5f * BALL_WIDTH;
+        final float startY = game.viewport.getWorldHeight() / 2 - 0.5f * BALL_HEIGHT;
+        ballSprite.setX(startX);
+        ballSprite.setY(startY);
+    }
+
+    private void checkRacketHit(Sprite racket) {
+        float ballX = ballSprite.getX();
+        float ballY = ballSprite.getY();
+        Rectangle ball = new Rectangle(ballX, ballY, BALL_WIDTH, BALL_HEIGHT);
+
+        float racketX = racket.getX();
+        float racketY = racket.getY();
+        Rectangle racketOne = new Rectangle(racketX, racketY, RACKET_WIDTH, RACKET_HEIGHT);
+
+        if (ball.overlaps(racketOne)) {
+            ballVelocityX *= -1;
+        }
+    }
+
+    /**
+     * The ball is floating towards player one, if it flies 
+     * to the left (neg direction)
+     */
+    private boolean isBallGoingToPlayerOne() {
+        return ballVelocityX < 0f;
+    }
+
     private void draw() {
         ScreenUtils.clear(Color.PINK);
         game.viewport.apply();
@@ -162,6 +241,11 @@ public class GameScreen implements Screen {
         racketPlayerOneSprite.draw(game.batch);
         racketPlayerTwoSprite.draw(game.batch);
         ballSprite.draw(game.batch);
+
+        game.font.draw(game.batch, 
+                       pointsPlayerOne + ":" + pointsPlayerTwo, 
+                       0f,
+                       game.WORLD_HEIGHT - 2f);
 
         game.batch.end();
     }
